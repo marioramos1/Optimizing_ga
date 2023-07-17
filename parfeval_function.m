4e0cca43f46c522a715ce5339961389459ac3502
marioramos1 <126744788+marioramos1@users.noreply.github.com>
2023-07-17 13:16:34 -0500

    Update parfeval_function.m

--- parfeval_function.m
+++ parfeval_function.m
@@ -1,11 +1,12 @@
 function [LCOH] = parfeval_function(x, varargin) 
-maxTime= 60; 
+% This code sets a limit to the time it takes while calling nSCA_fun. 
+maxTime= 60; % [seconds]
     g = parfeval( @nSCA_fun, 1, x);
     didFinish = wait(g, 'finished', maxTime);
+% If the function did not finish, the LCOH is set to a large number
     if ~didFinish
         cancel(g)
         LCOH = 10^10; 
-    else
+    else % the function pulls the output of nSCA_fun
         LCOH = fetchOutputs(g);
     end
-
