6139fbf8a6f94e30189765d85789951193367654
marioramos1 <126744788+marioramos1@users.noreply.github.com>
2023-07-17 13:11:34 -0500

    Update parfeval_function.m

--- parfeval_function.m
+++ parfeval_function.m
@@ -1,6 +1,6 @@
 function [LCOH] = parfeval_function(x, varargin) 
 maxTime= 60; 
-    g = parfeval( @nSCA_fun, 2, x);
+    g = parfeval( @nSCA_fun, 1, x);
     didFinish = wait(g, 'finished', maxTime);
     if ~didFinish
         cancel(g)
