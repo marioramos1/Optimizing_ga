fc516e2c5a2ce78cc1903842e2a9e4c92e3ce3bd
marioramos1 <126744788+marioramos1@users.noreply.github.com>
2023-07-26 09:59:08 -0500

    Update cancel_function.m

--- cancel_function.m
+++ cancel_function.m
@@ -4,7 +4,7 @@ function opt_percent_des = cancel_function(x)
 % optimizer to a variable called iterations. 
 
 maxTime= 120; % [seconds]
-g = parfeval(@(x)call_SAM_function([x(1),x(2),x(3),x(4)]),1,x);
+g = parfeval(@(x)call_SAM_function([x(1),x(2),x(3)]),1,x);
 didFinish = wait(g, 'finished', maxTime);
 % If the function did not finish, the prod inverse is set to a large number
 if ~didFinish
