3c672f861312b89d8c0acbe00d60044bb37a57f7
marioramos1 <126744788+marioramos1@users.noreply.github.com>
2023-07-26 10:16:32 -0500

    Update cancel_function.m

--- cancel_function.m
+++ cancel_function.m
@@ -1,7 +1,5 @@
 function opt_percent_des = cancel_function(x)
-% This code sets a limit to the time it takes while calling SAM; because of
-% its placement in the thread, this code also saves every iteration of the
-% optimizer to a variable called iterations. 
+% This code sets a limit to the time it takes while calling SAM. 
 
 maxTime= 120; % [seconds]
 g = parfeval(@(x)call_SAM_function([x(1),x(2),x(3)]),1,x);
